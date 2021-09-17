ca0d2e28c39ac6625455303b84dea7a50ba31367
Lachlan Morrison <lmorrison85@gmail.com>
2021-09-17 14:04:57 +0200

    Införde felsökning med TryParse, la till några fler felmeddelde och loopar. Uppdaterade utseende.

--- Roligheter_01/Program.cs
+++ Roligheter_01/Program.cs
@@ -7,8 +7,8 @@ namespace Roligheter_01
         static void Main(string[] args)
         {
             bool isRunning = true;
-            string myName = "", myYrke = "";
-            int myAge = 0;
+            string myName, myYrke = "", myAgeStr;
+            int myAgeInt;
 
             while (isRunning)
             {
@@ -18,15 +18,12 @@ namespace Roligheter_01
                 Console.WriteLine("Välj ett av följande alternativ: ");
                 Console.WriteLine("1: Ange Namn");
                 Console.WriteLine("2: Ange Ålder och Yrke");
-                Console.WriteLine("3: Matte");
-                Console.WriteLine("\n");
-                Console.WriteLine("0: Avsluta");
-
+                Console.WriteLine("3: Matte\n");
+                Console.WriteLine("0: Avsluta\n");
+                Console.Write("Svar: ");
                 string huvudMeny = Console.ReadLine();
                 bool menyLoop = true;
-
                 switch (huvudMeny)
-
                 {
                     case "1":
                         while (menyLoop)
@@ -36,17 +33,34 @@ namespace Roligheter_01
                             Console.WriteLine("----------------\n");
                             Console.Write("Ange ditt namn: "); //Console.Write kan man använda istället för WriteLine om man vill ha Readline på samma rad
                             myName = Console.ReadLine();
-                            Console.WriteLine("Ditt namn är {0}. Är detta korrekt? (Ja/Nej) ", myName); //Placeholders
-                            string menyValEtt = Console.ReadLine().ToLower(); // svaret till frågan ovan konverteras till Lowercase för att förenkla menyn
-
-                            if (menyValEtt == "ja" || menyValEtt == "j")
+                            if (string.IsNullOrWhiteSpace(myName)) //Ifall man inte skriver nåt. IsNullOrWhiteSpace täcker även IsNullOrEmpty
                             {
-                                menyLoop = false; //Bryter loss ur Namn loopen och återgår till huvudmenyn
+                                Console.Clear();
+                                Console.WriteLine("Uppgift 1 - Namn");
+                                Console.WriteLine("----------------\n");
+                                Console.WriteLine("Ett namn måste anges. Gör Om Gör Rätt.");
+                                Console.WriteLine("Tryck på valfri knapp för att Göra Om och Göra Rätt...");
+                                Console.ReadLine();
                             }
                             else
                             {
-                                menyLoop = (true); //Fortsätter Namn loopen
+                                Console.Clear();
+                                Console.WriteLine("Uppgift 1 - Namn");
+                                Console.WriteLine("----------------\n");
+                                Console.WriteLine("Ditt namn är {0}. Är detta korrekt? (Ja/Nej) \n", myName); //Placeholders
+                                Console.Write("Svar: ");
+                                string menyValEtt = Console.ReadLine().ToLower(); // svaret till frågan ovan konverteras till Lowercase för att förenkla menyn
+
+                                if (menyValEtt == "ja" || menyValEtt == "j")
+                                {
+                                    menyLoop = false; //Bryter loss ur Namn loopen och återgår till huvudmenyn
+                                }
+                                else
+                                {
+                                    menyLoop = (true); //Loopar om
+                                }
                             }
+
                         }
                         break;
 
@@ -57,21 +71,57 @@ namespace Roligheter_01
                             Console.WriteLine("Uppgift 2 - Ålder och Yrke");
                             Console.WriteLine("--------------------------\n");
                             Console.Write("Ange din Ålder: ");
-                            myAge = Convert.ToInt32(Console.ReadLine());
-                            Console.Write("Ange din Yrke: ");
-                            myYrke = Console.ReadLine();
-
-                            Console.WriteLine($"Din ålder är {myAge} och du jobbar som {myYrke}.\n"); //Interpolation     
-                            Console.Write("Är detta korrekt? (Ja/Nej) ");
-                            string menyValEtt = Console.ReadLine().ToLower(); // svaret till frågan ovan konverteras till Lowercase för att förenkla menyn
-
-                            if (menyValEtt == "ja" || menyValEtt == "j")
+                            myAgeStr = Console.ReadLine();
+                            bool agePass = int.TryParse(myAgeStr, out myAgeInt); //TryParse. Felmeddelande om det blir False
+                            if (agePass == true)
                             {
-                                menyLoop = false; //Bryter loss ur Namn loopen och återgår till huvudmenyn
+                                bool miniLoop = true;
+                                while (miniLoop)
+                                {
+                                    Console.Clear(); // Rensar Konsollen
+                                    Console.WriteLine("Uppgift 2 - Ålder och Yrke");
+                                    Console.WriteLine("--------------------------\n");
+                                    Console.Write("Ange din Yrke: ");
+                                    myYrke = Console.ReadLine();
+                                    if (string.IsNullOrWhiteSpace(myYrke)) //Ifall man inte skriver nåt. IsNullOrWhiteSpace täcker även IsNullOrEmpty
+                                    {
+                                        Console.Clear();
+                                        Console.WriteLine("Uppgift 2 - Ålder och Yrke");
+                                        Console.WriteLine("--------------------------\n");
+                                        Console.WriteLine("Yrke får inte vara tom. Gör Om Gör Rätt.");
+                                        Console.WriteLine("Tryck på valfri knapp för att Göra Om och Göra Rätt...");
+                                        Console.ReadLine();
+                                    }
+
+                                    Console.Clear();
+                                    Console.WriteLine("Uppgift 2 - Ålder och Yrke");
+                                    Console.WriteLine("--------------------------\n");
+                                    Console.WriteLine($"Din ålder är {myAgeInt} och du jobbar som {myYrke}.\n"); //Interpolation     
+                                    Console.WriteLine("Är detta korrekt? (Ja/Nej) \n");
+                                    Console.Write("Svar: ");
+                                    string menyValEtt = Console.ReadLine().ToLower(); // svaret till frågan ovan konverteras till Lowercase för att förenkla menyn
+
+                                    if (menyValEtt == "ja" || menyValEtt == "j")
+                                    {
+                                        miniLoop = false;
+                                        menyLoop = false; //Bryter loss ur Namn loopen och återgår till huvudmenyn
+                                    }
+                                    else
+                                    {
+                                        miniLoop = false;
+                                    }
+
+                                }
+
                             }
                             else
                             {
-                                menyLoop = (true);
+                                Console.Clear();
+                                Console.WriteLine("Uppgift 2 - Ålder och Yrke");
+                                Console.WriteLine("--------------------------1\n");
+                                Console.WriteLine("Ogiltiga tecken. Gör Om Gör Rätt.");
+                                Console.WriteLine("Tryck på valfri knapp för att Göra Om och Göra Rätt...");
+                                Console.ReadLine();
                             }
                         }
                         break;
@@ -79,76 +129,135 @@ namespace Roligheter_01
                     case "3":
                         while (menyLoop)
                         {
-                            Console.Clear(); // Rensar Konsollen
-                            double svar = 0;
+                            double svar = 0.0;
+                            string myFunctionStr = "", myNumOneStr = "", myNumTwoStr = "", miniContThree;
+                            double myNumOneDoub = 0.0, myNumTwoDoub = 0.0;
 
+                            Console.Clear(); // Rensar Konsollen
                             Console.WriteLine("Uppgift 3 - Miniräknare (för att jag saknar fantasi!!)");
                             Console.WriteLine("------------------------------------------------------\n");
-                            Console.Write("Siffra Ett: "); double myNumOne = double.Parse(Console.ReadLine());
-                            Console.Write("Funktion  : "); string myFunction = Console.ReadLine();
-                            Console.Write("Siffra Två: "); double myNumTwo = Convert.ToDouble(Console.ReadLine());
+                            Console.Write("Siffra Ett: ");
+                            myNumOneStr = Console.ReadLine();
+                            bool numOnePass = double.TryParse(myNumOneStr, out myNumOneDoub);
 
-                            if (myFunction == "/")
+                            if (numOnePass)
                             {
-                                svar = myNumOne / myNumTwo;
+
+                                {
+
+                                    Console.Write("Funktion  : ");
+                                    myFunctionStr = Console.ReadLine();
+
+                                    if (myFunctionStr == "/" || myFunctionStr == "*" || myFunctionStr == "-" || myFunctionStr == "+")
+                                    {
+                                        Console.Write("Siffra Två: ");
+
+                                        myNumTwoStr = Console.ReadLine();
+                                        bool myNumTwoPass = double.TryParse(myNumTwoStr, out myNumTwoDoub);
+
+                                        if (myNumTwoPass)
+                                        {
+
+                                            if (myFunctionStr == "/")
+                                            {
+                                                svar = myNumOneDoub / myNumTwoDoub;
+                                            }
+
+                                            else if (myFunctionStr == "*")
+                                            {
+                                                svar = myNumOneDoub * myNumTwoDoub;
+                                            }
+
+                                            else if (myFunctionStr == "-")
+                                            {
+                                                svar = myNumOneDoub - myNumTwoDoub;
+                                            }
+
+                                            else if (myFunctionStr == "+")
+                                            {
+                                                svar = myNumOneDoub + myNumTwoDoub;
+                                            }
+
+                                            //miniLoopThree = false;
+                                            Console.Clear(); // Rensar Konsollen
+                                            Console.WriteLine("Uppgift 3 - Miniräknare (för att jag saknar fantasi!!)");
+                                            Console.WriteLine("------------------------------------------------------\n");
+                                            Console.WriteLine($"{myNumOneDoub} {myFunctionStr} {myNumTwoDoub} = {svar}\n");
+                                            Console.WriteLine("1: Ny beräkning");
+                                            Console.WriteLine("0: Återgå till huvudmenyn \n");
+                                            Console.Write("Svar: ");
+                                            miniContThree = Console.ReadLine();
+                                            switch (miniContThree)
+                                            {
+                                                case "1":
+                                                    menyLoop = true;
+                                                    break;
+
+                                                case "0":
+                                                    menyLoop = false;
+                                                    Console.WriteLine("Wut");
+                                                    break;
+
+                                                default:
+                                                    Console.WriteLine("Felaktig Input (miniCont)");
+                                                    Console.WriteLine("Gör Om Gör Rätt");
+                                                    break;
+                                            }
+                                        }
+
+                                        else
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine("Uppgift 3 - Miniräknare (för att jag saknar fantasi!!)");
+                                            Console.WriteLine("------------------------------------------------------\n");
+                                            Console.WriteLine("Siffra Två måste vara en siffra");
+                                            Console.WriteLine("Felaktig Input (213). Gör Om Gör Rätt.");
+                                            Console.ReadLine();
+                                        }
+                                    }
+
+                                    else
+                                    {
+                                        Console.Clear();
+                                        Console.WriteLine("Uppgift 3 - Miniräknare (för att jag saknar fantasi!!)");
+                                        Console.WriteLine("------------------------------------------------------\n");
+                                        Console.WriteLine("Funktion måste vara en symbol: + - / *");
+                                        Console.WriteLine("Felaktig Input (225). Gör Om Gör Rätt.");
+                                        Console.ReadLine();
+                                    }
+                                }
                             }
 
-                            else if (myFunction == "*")
+                            else
                             {
-                                svar = myNumOne * myNumTwo;
+                                Console.Clear();
+                                Console.WriteLine("Uppgift 3 - Miniräknare (för att jag saknar fantasi!!)");
+                                Console.WriteLine("------------------------------------------------------\n");
+                                Console.WriteLine("Siffra Ett måste vara en siffra");
+                                Console.WriteLine("Felaktig Input(237). Gör Om Gör Rätt.");
+                                Console.ReadLine();
                             }
-
-                            else if (myFunction == "-")
-                            {
-                                svar = myNumOne - myNumTwo;
-                            }
-
-                            else if (myFunction == "+")
-                            {
-                                svar = myNumOne + myNumTwo;
-                            }
-                           
-                            Console.Clear(); // Rensar Konsollen
-                            Console.WriteLine($"{myNumOne} {myFunction} {myNumTwo} = {svar}\n");
-                            Console.WriteLine("1: Ny beräkning");
-                            Console.WriteLine("0: Återgå till huvudmenyn");
-                            string miniCont = Console.ReadLine();
-
-                            switch (miniCont)
-                            {
-                                case "1":
-                                    break;
-
-                                case "0":
-                                    menyLoop = false;
-                                    break;
-                            }
-
                         }
-                        break; //Avslutar Programmet
+                        break;
 
                     case "0":
-                            isRunning = false;
+                        isRunning = false;
                         break;
 
                     default:
-                        while (menyLoop)
-                        {
-                            Console.Clear(); // Rensar Konsollen
-                            Console.WriteLine("Felaktig Input. Var god försök igen.\n");
-                            Console.WriteLine("Tryck på valfri knapp för att återgå till huvudmenyn.");
-                            string miniCont = Console.ReadLine();
-
-                            switch (miniCont)
-                            {
-                                default:
-                                    menyLoop = false;
-                                    break;
-                            }
-                        }
+                        Console.Clear(); // Rensar Konsollen
+                        Console.WriteLine("Roligheter 01");
+                        Console.WriteLine("-------------\n");
+                        Console.WriteLine("Felaktig Input. Var god försök igen.\n");
+                        Console.WriteLine("Tryck på valfri knapp för att återgå till huvudmenyn.");
+                        Console.ReadLine();
                         break;
                 }
             }
         }
     }
 }
+
+
+
+
